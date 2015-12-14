994ce26b98929e476efa6385bc4368ec73c224a8
Oscar Renalias <oscar@renalias.net>
2015-12-14 16:56:19 +0200

    Beginning of the support for AWS.

---
+++ provisioning/aws/provision.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#!/bin/bash
+
+if [[ "$AWS_ACCESS_KEY_ID" == "" || "$AWS_SECRET_ACCESS_KEY" == "" || "$AWS_DEFAULT_REGION" == "" || "" ]]; then
+	echo "ERROR: please provide environment variables AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_DEFAULT_REGION"
+	exit 1
+fi
+
+if [[ "$1" == "" ]] ; then
+	echo "usage: $0 [plan/apply/destroy..]"
+	exit 1
+fi
+
+TERRAFORM_ACTION=$1
+
+terraform $1
+if [[ $? -ne 0 ]] ; then
+  echo "ERROR: Terraform failed"
+  exit 1
+fi
+
+echo 'Done'
+exit 0
---
+++ provisioning/aws/terraform.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+    # access key, secret and region will be retrieved from environment variables
+}
+
+resource "aws_vpc" "tldr-vpc" {
+    cidr_block = "10.0.0.0/16"
+    instance_tenancy = "default"
+    enable_dns_support = "true"
+    enable_dns_hostnames = "true" 
+
+    tags {
+        Name = "tldr-vpc"
+    }
+}
+
+resource "aws_internet_gateway" "tldr-igw" {
+    vpc_id = "${aws_vpc.tldr-vpc.id}"
+
+    tags {
+        Name = "tldr-igw"
+    }
+}
+
+resource "aws_subnet" "tldr-subnet-1" {
+    vpc_id = "${aws_vpc.tldr-vpc.id}"
+    cidr_block = "10.0.1.0/24"
+    
+    # This seems to be optional, we leave out for now
+    #availability_zone = "${var.availability_zone}"
+    
+    map_public_ip_on_launch = "false"
+
+    tags {
+        Name = "tldr-subnet-1"
+    }
+}
+
+resource "aws_route_table" "tldr-route-table" {
+    vpc_id = "${aws_vpc.tldr-vpc.id}"
+    
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.tldr-igw.id}"
+    }
+
+    tags {
+        Name = "tldr-route-table"
+    }
+}
