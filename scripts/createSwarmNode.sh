cd777e9da84f97ee3a4ae9dc11577014364b611c
Oscar Renalias <oscar@renalias.net>
2016-01-11 12:07:07 +0200

    Make sure that containers are called the same across local and AWS deployments.

--- scripts/createSwarmNode.sh
+++ scripts/createSwarmNode.sh
@@ -23,7 +23,7 @@ if [ isAWS ]; then
       print "Creating network tldr-overlay"
       docker $(docker-machine config $NAME) network create --driver overlay tldr-overlay
       print "Starting master consul"
-      docker $(docker-machine config $NAME) run -d -p 172.17.0.1:53:53 -p 172.17.0.1:53:53/udp -p 8500:8500 --name $NAME-consul --net tldr-overlay $REGISTRY/consul -server -bootstrap-expect 1
+      docker $(docker-machine config $NAME) run -d -p 172.17.0.1:53:53 -p 172.17.0.1:53:53/udp -p 8500:8500 --name tldr-swarm-$1-consul --net tldr-overlay $REGISTRY/consul -server -bootstrap-expect 1
     else
       print "$NAME already running"
       exit 1
@@ -31,7 +31,7 @@ if [ isAWS ]; then
   else
     NAME="$SWARM_MACHINE_NAME_PREFIX-$1"
     # For some reason the join only works with an IP address, not with hostname
-    OVERLAY_CONSUL=$(docker $(docker-machine config $SWARM_MACHINE_NAME_PREFIX-0) inspect -f '{{(index .NetworkSettings.Networks "tldr-overlay").IPAddress}}' tldr-swarm-0-aws-consul)
+    OVERLAY_CONSUL=$(docker $(docker-machine config $SWARM_MACHINE_NAME_PREFIX-0) inspect -f '{{(index .NetworkSettings.Networks "tldr-overlay").IPAddress}}' tldr-swarm-0-consul)
     if ! docker-machine inspect $NAME &> /dev/null; then
       print "Creating swarm node with the name '$NAME' to AWS, label: $2"
       docker-machine create --driver amazonec2 \
@@ -39,7 +39,7 @@ if [ isAWS ]; then
          --engine-opt="cluster-store=consul://$CONSUL:8500" --engine-opt="cluster-advertise=eth0:2376" $OPTIONS --engine-insecure-registry="$REGISTRY" \
          --amazonec2-ami="$TLDR_DOCKER_MACHINE_AMI" --amazonec2-security-group="$TLDR_NODE_SG_NAME" $NAME
       print "Starting slave consul"
-      docker $(docker-machine config $NAME) run -d -p 172.17.0.1:53:53 -p 172.17.0.1:53:53/udp -p 8500:8500 --name $NAME-consul --net tldr-overlay $REGISTRY/consul -join $OVERLAY_CONSUL
+      docker $(docker-machine config $NAME) run -d -p 172.17.0.1:53:53 -p 172.17.0.1:53:53/udp -p 8500:8500 --name tldr-swarm-$1-consul --net tldr-overlay $REGISTRY/consul -join $OVERLAY_CONSUL
     else
       print "$NAME already running"
       exit 1
@@ -57,7 +57,7 @@ else
       print "Creating network tldr-overlay"
       docker $(docker-machine config $NAME) network create --driver overlay tldr-overlay
       print "Starting master consul"
-      docker $(docker-machine config $NAME) run -d -p 172.17.0.1:53:53 -p 172.17.0.1:53:53/udp -p 8500:8500 --name $NAME-consul --net tldr-overlay $REGISTRY/consul -server -bootstrap-expect 1
+      docker $(docker-machine config $NAME) run -d -p 172.17.0.1:53:53 -p 172.17.0.1:53:53/udp -p 8500:8500 --name tldr-swarm-$1-consul --net tldr-overlay $REGISTRY/consul -server -bootstrap-expect 1
     else
       print "$NAME already running"
       exit 1
@@ -70,7 +70,7 @@ else
       print "Creating swarm node with the name '$NAME' locally, label: $2"
       docker-machine create --driver virtualbox --swarm --swarm-discovery consul://$CONSUL:8500 --engine-opt="cluster-store=consul://$CONSUL:8500" --engine-opt="cluster-advertise=eth1:2376" $OPTIONS --engine-insecure-registry=$REGISTRY $NAME
       print "Starting slave consul"
-      docker $(docker-machine config $NAME) run -d -p 172.17.0.1:53:53 -p 172.17.0.1:53:53/udp -p 8500:8500 --name $NAME-consul --net tldr-overlay $REGISTRY/consul -join $OVERLAY_CONSUL
+      docker $(docker-machine config $NAME) run -d -p 172.17.0.1:53:53 -p 172.17.0.1:53:53/udp -p 8500:8500 --name tldr-swarm-$1-consul --net tldr-overlay $REGISTRY/consul -join $OVERLAY_CONSUL
     else
       print "$NAME already running"
       exit 1
@@ -80,5 +80,5 @@ else
 fi
 eval $(docker-machine env $NAME)
 print "Starting registrator"
-docker run -d -v /var/run/docker.sock:/tmp/docker.sock -h registrator --name $NAME-registrator --net tldr-overlay $REGISTRY/registrator -internal consul://$NAME-consul:8500
+docker run -d -v /var/run/docker.sock:/tmp/docker.sock -h registrator --name tldr-swarm-$1-registrator --net tldr-overlay $REGISTRY/registrator -internal consul://tldr-swarm-$1-consul:8500
 print "Started a new node with IP \e[31m$(docker-machine ip $NAME)"
