81785b2244feaf78f705862646ee2f2c71339454
Oscar Renalias <oscar@renalias.net>
2016-02-25 17:00:48 +0200

    Fixed the info script, fixes #8

---
+++ bin/info.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copyright 2016 The Lightweight Docker Runtime contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TLDR_ROOT=$(dirname ${BASH_SOURCE[0]})/..
+TLDR_BIN=$TLDR_ROOT/bin
+source $TLDR_BIN/utils.sh
+
+detect_provider
+info "Using provider: ${TLDR_PROVIDER}"
+source $TLDR_BIN/providers/$TLDR_PROVIDER/provider.sh
+
+PROMETHEUS=http://$(docker-machine ip $INFRA_MACHINE_NAME):9090
+PROMDASH=http://$(docker-machine ip $INFRA_MACHINE_NAME):3000
+KIBANA=http://$(docker-machine ip $INFRA_MACHINE_NAME):5601
+
+info "Kibana is available at: $KIBANA"
+info "Prometheus is available at: $PROMETHEUS"
+info "PromDash is available at: $PROMDASH"
--- info.sh
+++ info.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-source $(dirname ${BASH_SOURCE[0]})/scripts/docker-functions.sh
-source $(dirname ${BASH_SOURCE[0]})/scripts/nodeNames.sh
-if [ $AWS_ACCESS_KEY_ID ]; then
-  REGISTRY=$(docker-machine inspect --format='{{.Driver.PrivateIPAddress}}' $REGISTRY_MACHINE_NAME-aws):5000
-  PROMETHEUS=http://$(docker-machine ip $INFRA_MACHINE_NAME-aws):9090
-  KIBANA=http://$(docker-machine ip $INFRA_MACHINE_NAME-aws):5601
-  eval $(docker-machine env --swarm $SWARM_MACHINE_NAME_PREFIX-0-aws)
-else
-  REGISTRY=$(docker-machine ip $REGISTRY_MACHINE_NAME):5000
-  PROMETHEUS=http://$(docker-machine ip $INFRA_MACHINE_NAME):9090
-  KIBANA=http://$(docker-machine ip $INFRA_MACHINE_NAME):5601
-  eval $(docker-machine env --swarm $SWARM_MACHINE_NAME_PREFIX-0)
-fi
-print "Kibana is available at \e[31m$KIBANA"
-print "Prometheus is available at \e[31m$PROMETHEUS"
-print "Registry is available at \e[31m$REGISTRY"
+bin/info.sh
